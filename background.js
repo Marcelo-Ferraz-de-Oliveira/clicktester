647c1929bcca17d489c9ce4afd16e58734606d47
Marcelo Ferraz de Oliveira <marcelo.ferraz@ifg.edu.br>
2023-05-29 14:38:27 -0300

    remove executeScript - o content.js já é especificado no manifest

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
-chrome.tabs.query({active: true}, tabs => {
-  const tabId = tabs[0].id;
-  chrome.scripting.executeScript({
-    target: { tabId: tabId },
-    files: ["content.js"],
-  });
-});
+// chrome.tabs.query({active: true}, tabs => {
+//   const tabId = tabs[0].id;
+//   chrome.scripting.executeScript({
+//     target: { tabId: tabId },
+//     files: ["content.js"],
+//   });
+// });
